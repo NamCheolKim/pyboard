f3504f77925033a7d4d1d4ee0068fad7cc41c847
NamCheolKim <pkeowkd@naver.com>
2021-04-22 02:18:09 +0900

    gitignore추가, 질문 리스트, 질문상세, 답변기능 추가

--- pyboard/urls.py
+++ pyboard/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = "pyboard"
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("answer/create/<int:pk>/", views.answer_create, name="answer"),
 ]
--- pyboard/views.py
+++ pyboard/views.py
@@ -1,5 +1,32 @@
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from . import models
+from django.utils import timezone
 
 
-def index(request):
-    return HttpResponse("안녕하세요. pyboard입니다.")
+class IndexView(ListView):
+    """PyBoard List Definition"""
+
+    def get_queryset(self):
+        return models.Question.objects.order_by("-create_date")
+
+
+class DetailView(DetailView):
+    """PyBoard Detail Definition"""
+
+    model = models.Question
+
+
+def answer_create(request, pk):
+    """PyBoard Answer Definition"""
+
+    question = get_object_or_404(models.Question, pk=pk)
+    answer = models.Answer(
+        question=question,
+        content=request.POST.get("content"),
+        create_date=timezone.now(),
+    )
+    answer.save()
+
+    return redirect(reverse("pyboard:detail", kwargs={"pk": question.pk}))
