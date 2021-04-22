a85fbe3b93a3d313027c9f6fa72ab76f07959e97
NamCheolKim <pkeowkd@naver.com>
2021-04-23 02:40:42 +0900

    질문생성, 답변 기능 수정

--- pyboard/views.py
+++ pyboard/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from . import models
 from django.utils import timezone
+from . import forms
 
 
 class IndexView(ListView):
@@ -22,11 +22,38 @@ def answer_create(request, pk):
     """PyBoard Answer Definition"""
 
     question = get_object_or_404(models.Question, pk=pk)
-    answer = models.Answer(
-        question=question,
-        content=request.POST.get("content"),
-        create_date=timezone.now(),
-    )
-    answer.save()
 
-    return redirect(reverse("pyboard:detail", kwargs={"pk": question.pk}))
+    if request.method == "POST":
+        form = forms.AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save()
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+
+            return redirect(reverse("pyboard:detail", kwargs={"pk": question.pk}))
+    else:
+        form = forms.AnswerForm()
+
+    context = {"question": question, "form": form}
+
+    return render(request, "pyboard/question_detail.html", context)
+
+
+def question_create(request):
+    """PyBoard Question Definition"""
+
+    if request.method == "POST":
+        form = forms.QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save()
+            question.create_date = timezone.now()
+            question.save()
+
+            return redirect(reverse("pyboard:index"))
+    else:
+        form = forms.QuestionForm()
+
+    context = {"form": form}
+
+    return render(request, "pyboard/question_form.html", context)
