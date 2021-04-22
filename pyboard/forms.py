7ca59c964a130d670809689ba3e10bc6c62820cd
NamCheolKim <pkeowkd@naver.com>
2021-04-23 02:39:08 +0900

    question form, answer form 추가

---
+++ pyboard/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from . import models
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = models.Question
+        fields = ("subject", "content")
+        labels = {
+            "subject": "제목",
+            "content": "내용",
+        }
+
+    def save(self):
+        question = super().save(commit=False)
+        return question
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = models.Answer
+        fields = ("content",)
+        labels = {"content": "답변내용"}
+
+    def save(self):
+        answer = super().save(commit=False)
+        return answer
