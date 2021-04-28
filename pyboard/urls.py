8a0f4addfd85fd33cd055454f3708ceea9f61bc1
NamCheolKim <pkeowkd@naver.com>
2021-04-29 01:36:14 +0900

    게시물 수정 삭제 기능 추가 글쓴이 표시 추가

--- pyboard/urls.py
+++ pyboard/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("answer/create/<int:pk>/", views.answer_create, name="answer"),
     path("question/create/", views.question_create, name="question"),
+    path("question/modify/<int:pk>/", views.question_modify, name="question_modify"),
+    path("question/delete/<int:pk>/", views.question_delete, name="question_delete"),
+    path("answer/modify/<int:pk>/", views.answer_modify, name="answer_modify"),
+    path("answer/delete/<int:pk>/", views.answer_delete, name="answer_delete"),
 ]
--- pyboard/views.py
+++ pyboard/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.core.paginator import Paginator
@@ -7,7 +9,7 @@ from . import forms
 
 
 class IndexView(ListView):
-    """PyBoard List Definition"""
+    """PyBoard IndexView Definition"""
 
     def get(self, request):
         # 조회
@@ -26,13 +28,14 @@ class IndexView(ListView):
 
 
 class DetailView(DetailView):
-    """PyBoard Detail Definition"""
+    """PyBoard DetailView Definition"""
 
     model = models.Question
 
 
+@login_required(login_url="common:login")
 def answer_create(request, pk):
-    """PyBoard Answer Definition"""
+    """PyBoard Answer Create Definition"""
 
     question = get_object_or_404(models.Question, pk=pk)
 
@@ -40,6 +43,7 @@ def answer_create(request, pk):
         form = forms.AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save()
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -53,13 +57,15 @@ def answer_create(request, pk):
     return render(request, "pyboard/question_detail.html", context)
 
 
+@login_required(login_url="common:login")
 def question_create(request):
-    """PyBoard Question Definition"""
+    """PyBoard Question Create Definition"""
 
     if request.method == "POST":
         form = forms.QuestionForm(request.POST)
         if form.is_valid():
             question = form.save()
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
 
@@ -70,3 +76,84 @@ def question_create(request):
     context = {"form": form}
 
     return render(request, "pyboard/question_form.html", context)
+
+
+@login_required(login_url="common:login")
+def question_modify(request, pk):
+    """ PyBoard Question Modify Definition """
+
+    question = get_object_or_404(models.Question, pk=pk)
+
+    if request.user != question.author:
+        messages.error(request, "수정권한이 없습니다.")
+        return redirect(reverse("pyboard:detail", kwargs={"pk": question.pk}))
+
+    if request.method == "POST":
+        form = forms.QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save()
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect(reverse("pyboard:detail", kwargs={"pk": question.pk}))
+    else:
+        form = forms.QuestionForm(instance=question)
+
+    context = {"form": form}
+    return render(request, "pyboard/question_form.html", context)
+
+
+@login_required(login_url="common:login")
+def question_delete(request, pk):
+    """ Pyboard Question Delete Definition """
+
+    question = get_object_or_404(models.Question, pk=pk)
+
+    if request.user != question.author:
+        messages.error(request, "삭제권한이 없습니다.")
+        return redirect(reverse("pyboard:detail", kwargs={"pk": question.pk}))
+
+    question.delete()
+    return redirect(reverse("pyboard:index"))
+
+
+@login_required(login_url="common:login")
+def answer_modify(request, pk):
+    """ Pyboard Answer Modify Definition """
+
+    answer = get_object_or_404(models.Answer, pk=pk)
+
+    if request.user != answer.author:
+        messages.error(request, "수정권한이 없습니다.")
+        return redirect(reverse("pyboard:detail", kwargs={"pk": answer.question.pk}))
+
+    if request.method == "POST":
+        form = forms.AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save()
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+
+            return redirect(
+                reverse("pyboard:detail", kwargs={"pk": answer.question.pk})
+            )
+    else:
+        form = forms.AnswerForm(instance=answer)
+
+    context = {"answer": answer, "form": form}
+
+    return render(request, "pyboard/answer_form.html", context)
+
+
+@login_required(login_url="common:login")
+def answer_delete(request, pk):
+    """ Pyboard Answer Delete Definition """
+
+    answer = get_object_or_404(models.Answer, pk=pk)
+
+    if request.user != answer.author:
+        messages.error(request, "삭제권한이 없습니다.")
+    else:
+        answer.delete()
+    return redirect(reverse("pyboard:detail", kwargs={"pk": answer.question.pk}))
